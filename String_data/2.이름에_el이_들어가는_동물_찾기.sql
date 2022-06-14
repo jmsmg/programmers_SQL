44b2641ecef28a7ab01d51c25ed3ea1ba69bab1f
jmsmg <jmsmg1@me.com>
2022-06-14 23:08:15 +0900

    다른 방법

--- String_data/2.이름에_el이_들어가는_동물_찾기.sql
+++ String_data/2.이름에_el이_들어가는_동물_찾기.sql
@@ -1,3 +1,7 @@
+-- SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
+-- WHERE ANIMAL_TYPE = 'DOG' AND INSTR (NAME, 'el')
+-- ORDER BY NAME
+
 SELECT animal_id, name
 FROM animal_ins
 WHERE animal_type = 'Dog' AND name LIKE '%el%'
